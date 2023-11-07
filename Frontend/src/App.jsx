93a3d1b572b9be19b3421a459b3f1aa44c72ab45
xKeros <johaquiros968@gmail.com>
2023-11-07 15:46:28 -0600

    Add: Layout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,18 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import Scanner from './Components/Scanner'
+import React from "react";
+import Layout from "./Pages/Layout";
+import { BrowserRouter, Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Scanner />
-  )
-}
+    <div>
+      <BrowserRouter>
+        <Router>
+          <Route path="/" component={Layout} />
+        </Router>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ Frontend/src/Pages/ListaInventarios.jsx
@@ -0,0 +1,7 @@
+
+
+export const ListaInventarios = () => {
+  return (
+    <div></div>
+  )
+}
