f48edf94f4801faa1c6f7a9e2576646d8cfcd320
xKeros <johaquiros968@gmail.com>
2023-11-09 13:50:58 -0600

    Add: Layout

--- Frontend/src/Pages/Layout.jsx
+++ Frontend/src/Pages/Layout.jsx
@@ -1,24 +1,28 @@
-import { Button, Typography } from '@mui/material'
-import '../Styles/layout.css'
+import "../Styles/layout.css"
 
 const Layout = () => {
   return (
     <div>
-      <main>
-        <div className="container">
-          <div className="info">
-            <Typography variant='h1'>
-              Lunaca S.A
-            </Typography>
+      <div className="banner">
+        <div className="slider">
+          <img src="https://sigelmanassociates.com/wp-content/uploads/2022/04/California-Commercial-Truck-Axle-Weight-Limits-2022.jpg" alt="Banner 1" id="sliderImg"/>
+        </div>
+        <div className="overlay">
+          <div className="navbar">
+            <div className="logo">
+              <img src="https://imgur.com/JtfM5BG.png" alt="Logo" />
+            </div>
           </div>
-          <div className="background">            <img
-            src="https://www.vascongada.com/wp-content/uploads/2020/04/transport-4916922_1280.jpg"
-            alt="Background"
-            style={{ width: '100%' }}
-          />
+          <div className="content">
+            <h1>Inventarios Lunaca S.A</h1>
+            <h3>Sistema de control de inventarios para Lunaca S.A</h3>
+          </div>
+          <div>
+            <a href="/crear"><button type="button">Escanear Productos</button></a>
+            <a href="/lista"><button type="button">Ver Inventarios</button></a>
           </div>
         </div>
-      </main>
+      </div>
     </div>
   )
 }
